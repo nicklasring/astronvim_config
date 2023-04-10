8c5da76484c22c3bdf779b8a440b572ad0698cb0
nicklasring <nicklas.ring@verendus.se>
2023-04-10 11:46:58 +0200

    Golang plugin and lua lsp

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,13 @@
 return {
+  "ray-x/go.nvim",
+  dependencies = {
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function() require("go").setup() end,
+  event = { "CmdlineEnter" },
+  ft = { "go", "gomod" },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
